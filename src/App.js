1f777d3846a4c903ee9ac6fef7854624ebade6a2
Shamsul Arifin <arifinbd6@gmail.com>
2022-11-25 17:13:53 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -11,6 +11,13 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 export default function App() {
   const [pokemon, setPokemon] = useState('pikachu');
@@ -43,6 +50,24 @@ export default function App() {
 
   return (
     <div className="App">
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" sx={{ background: '#790000' }}>
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" sx={{ flexGrow: 1 }}>
+              PokeWiki
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
       <h1>Wellcome to PokeWiki</h1>
       <form onSubmit={handdleSubmit}>
         <label>
